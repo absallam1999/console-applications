7d81bc267f7e5bdc7a03fc3c860304154f8ff40a
Mohamed Badr <115354829+absallam1999@users.noreply.github.com>
2025-02-09 10:50:58 +0200

    V1

---
+++ c/1.c
@@ -0,0 +1,16 @@
+/* Write a C code that will print your short biography.
+    ** Full Name
+    ** Birth Year
+    ** Faculty
+    ** Graduation Year
+*/
+
+#include <stdio.h>
+
+void main()
+{
+    printf("Full Name: Mohamed Badr\n");
+    printf("Birth Year: Jan, 1999\n");
+    printf("Faculty: Computer Science\n");
+    printf("Graduation Year: 2022\n");
+}
