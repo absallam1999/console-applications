247e3d036df7611d6eacdfbeff9dc272b72f5d9e
Mohamed Badr <115354829+absallam1999@users.noreply.github.com>
2025-02-09 10:51:19 +0200

    2.c

---
+++ c/2.c
@@ -0,0 +1,14 @@
+// Write a C code that will ask the user to enter a value then print it.
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+    
+    printf("Please Enter Value: \n");
+    scanf("%d", &x);
+    printf("The Value you Enterd is: %d", x);
+
+    return 0;
+}
